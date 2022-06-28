afa1b79a3a12d6ee92b7464ab756833da733b069
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-28 19:01:43 +0530

    Add UC1 to Creat Simple Linked List

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 list = new Class1();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
